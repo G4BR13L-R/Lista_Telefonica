05ea7c058215f176700917a9e993941b17c5aea8
Gabriel Silva de Rezende <802.239@alunos.unigran.br>
2022-09-26 11:13:35 -0400

    Pequenos ajustes

--- app/src/main/java/com/gabriel/listatelefonicatrabalho/MainActivity.java
+++ app/src/main/java/com/gabriel/listatelefonicatrabalho/MainActivity.java
@@ -21,20 +21,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    EditText campoNome;
-    EditText campoDataNascimento;
-    EditText campoTelefone;
-
+    EditText editTextNome;
+    EditText editTextTelefone;
+    EditText editTextDataNascimento;
     Button botaoSalvar;
-
-    ListView listagemDados;
+    ListView listViewTelefones;
     List<Telefone> listaTelefonica;
     ArrayAdapter adapter;
-
     DBTelefone DBtelefone;
     Telefone telefone;
-
-    Boolean editarTelefone;
+    Boolean validarEdicao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,38 +40,35 @@ public class MainActivity extends AppCompatActivity {
         DBHelper db = new DBHelper(MainActivity.this);
         DBtelefone = new DBTelefone(db);
 
-        campoNome = findViewById(R.id.campoNome);
-        campoDataNascimento = findViewById(R.id.campoDataNascimento);
-        campoTelefone = findViewById(R.id.campoTelefone);
-        botaoSalvar = findViewById(R.id.botaoSalvar);
-        listagemDados = findViewById(R.id.listagemDados);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextTelefone = findViewById(R.id.editTextTelefone);
+        editTextDataNascimento = findViewById(R.id.editTextDataDeNascimento);
+        botaoSalvar = findViewById(R.id.buttonSalvar);
+        listViewTelefones = findViewById(R.id.listViewTelefones);
 
         listaTelefonica = new ArrayList<>();
         adapter = new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listaTelefonica);
 
-        listagemDados.setAdapter(adapter);
-        DBTelefone.listar(listaTelefonica);
+        listViewTelefones.setAdapter(adapter);
+        DBtelefone.listar(listaTelefonica);
 
-        editarTelefone = false;
-        campoNome.requestFocus();
+        validarEdicao = false;
 
-        acaoComponentes();
+        comandosComponentes();
     }
 
     @Override
     public void onBackPressed() {
         telefone = null;
-        editarTelefone = false;
-        campoNome.setText("");
-        campoNome.requestFocus();
-        campoDataNascimento.setText("");
-        campoTelefone.setText("");
-
+        validarEdicao = false;
+        editTextNome.setText("");
+        editTextTelefone.setText("");
+        editTextDataNascimento.setText("");
         Toast.makeText(MainActivity.this, "Cancelado com Sucesso!", Toast.LENGTH_LONG).show();
     }
 
-    private void acaoComponentes() {
-        listagemDados.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+    private void comandosComponentes() {
+        listViewTelefones.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 new AlertDialog.Builder(view.getContext())
@@ -83,14 +76,12 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("Editar", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int j) {
-                                editarTelefone = true;
-
+                                validarEdicao = true;
                                 telefone = new Telefone();
                                 telefone.setId(listaTelefonica.get(i).getId());
-
-                                campoNome.setText(listaTelefonica.get(i).getNome());
-                                campoDataNascimento.setText(listaTelefonica.get(i).getDataNascimento());
-                                campoTelefone.setText(listaTelefonica.get(i).getTelefone());
+                                editTextNome.setText(listaTelefonica.get(i).getNome());
+                                editTextTelefone.setText(listaTelefonica.get(i).getTelefone());
+                                editTextDataNascimento.setText(listaTelefonica.get(i).getDataNascimento());
                             }
                         })
                         .setNegativeButton("Remover", new DialogInterface.OnClickListener() {
@@ -102,10 +93,8 @@ public class MainActivity extends AppCompatActivity {
                                             @Override
                                             public void onClick(DialogInterface dialogInterface, int k) {
                                                 DBtelefone.remover(listaTelefonica.get(i).getId());
-
                                                 DBtelefone.listar(listaTelefonica);
                                                 adapter.notifyDataSetChanged();
-
                                                 Toast.makeText(MainActivity.this, "Removido com Sucesso!", Toast.LENGTH_LONG).show();
                                             }
                                         })
@@ -121,36 +110,30 @@ public class MainActivity extends AppCompatActivity {
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (campoNome.getText().toString().isEmpty() || campoDataNascimento.getText().toString().isEmpty() || campoTelefone.getText().toString().isEmpty()) {
+                if (editTextNome.getText().toString().isEmpty() || editTextTelefone.getText().toString().isEmpty()|| editTextDataNascimento.getText().toString().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Dados Inválidos!", Toast.LENGTH_SHORT).show();
                 } else {
-                    if (editarTelefone == false) {
+                    if (validarEdicao == false) {
                         telefone = new Telefone();
                     }
-
-                    telefone.setNome(campoNome.getText().toString());
-                    telefone.setDataNascimento(campoDataNascimento.getText().toString());
-                    telefone.setTelefone(campoTelefone.getText().toString());
-
-                    if (editarTelefone) {
+                    telefone.setNome(editTextNome.getText().toString());
+                    telefone.setTelefone(editTextTelefone.getText().toString());
+                    telefone.setDataNascimento(editTextDataNascimento.getText().toString());
+                    if (validarEdicao) {
                         DBtelefone.editar(telefone);
-
                         Toast.makeText(MainActivity.this, "Editado com Sucesso!", Toast.LENGTH_LONG).show();
                     } else {
                         DBtelefone.inserir(telefone);
-
                         Toast.makeText(MainActivity.this, "Salvo com Sucesso!", Toast.LENGTH_LONG).show();
                     }
 
                     DBtelefone.listar(listaTelefonica);
                     adapter.notifyDataSetChanged();
-
                     telefone = null;
-                    editarTelefone = false;
-                    campoNome.setText("");
-                    campoNome.requestFocus();
-                    campoDataNascimento.setText("");
-                    campoTelefone.setText("");
+                    validarEdicao = false;
+                    editTextNome.setText("");
+                    editTextTelefone.setText("");
+                    editTextDataNascimento.setText("");
                 }
             }
         });
